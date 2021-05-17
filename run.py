37615ae2272ef2a4c34a789105a0b4712617bb08
GuiPimenta--Dev <guialvespimenta27@gmail.com>
2021-05-16 23:53:28 -0300

    t1

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from api import app
-import os
-PORT = int(os.environ.get('PORT', 5000))
-# app.run(host="0.0.0.0", port=80,debug=True)
-app.run(host="0.0.0.0", port=PORT,debug=False)
+# import os
+# PORT = int(os.environ.get('PORT', 5000))
+if __name__ == '__main__':
+  app.run(host="0.0.0.0", port=80,debug=True)
+# app.run(host="0.0.0.0", port=PORT,debug=False)
