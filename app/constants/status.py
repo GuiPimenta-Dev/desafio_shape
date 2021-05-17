da8f5e40d7e92333a48b439107b93698f41b8197
GuiPimenta--Dev <guialvespimenta27@gmail.com>
2021-05-16 22:52:13 -0300

    upload heroku

--- app/constants/status.py
+++
@@ -1,5 +0,0 @@
-from typing import NamedTuple
-
-class StatusEnum(NamedTuple):
-    ACTIVE = 'active'
-    INACTIVE = 'inactive'
--- run.py
+++ run.py
@@ -1,3 +1,3 @@
-# from app import app
+from api import app
 
-# app.run(host="0.0.0.0", port=80,debug=True)
+app.run(host="0.0.0.0", port=80,debug=True)
