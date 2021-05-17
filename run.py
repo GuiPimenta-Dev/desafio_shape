81d6ebddbff70cb4b9c4e906cffa168b377acc26
GuiPimenta--Dev <guialvespimenta27@gmail.com>
2021-05-16 23:44:35 -0300

    asuhsauhsa

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from api import app
-
+import os
+PORT = int(os.environ.get('PORT', 5000))
 # app.run(host="0.0.0.0", port=80,debug=True)
-app.run(host="0.0.0.0", port=80,debug=False)
+app.run(host="0.0.0.0", port=PORT,debug=False)
